8bc80466b2193964777a47839ff88e0b399104df
EsotericZ <cjsand03@gmail.com>
2021-10-26 21:34:43 -0600

    solved week 03 - 02 and 04

---
+++ week03/04-Stu_Hello-Variable/solved/script.js
@@ -0,0 +1,15 @@
+var personName = 'CJ';
+var pets = 74;
+var funFact = 'Ducks are great';
+
+console.log("My Name is " + personName);
+console.log("I have " + pets + " pets");
+console.log("Fun Fact: " + funFact);
+
+personName = 'Kirtly'
+pets = 2;
+funFact = "Also doesn't like old dude";
+
+console.log("My Name is " + personName);
+console.log("I have " + pets + " pets");
+console.log("Fun Fact: " + funFact);
