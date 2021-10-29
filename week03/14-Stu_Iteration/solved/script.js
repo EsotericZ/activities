da1cdaac717f8c88b276ebd6fe1428a67bb51a53
EsotericZ <cjsand03@gmail.com>
2021-10-28 21:19:36 -0600

    solved week 03 - 14

---
+++ week03/14-Stu_Iteration/solved/script.js
@@ -0,0 +1,16 @@
+//WRITE YOUR CODE BELOW
+var students = [
+    'Jack',
+    'Zero',
+    'Oggie Boogie',
+    'Sally',
+    'Shock'
+];
+
+// LENGTH OF ARRAY
+console.log(students.length);
+
+// FOR LOOP
+for (let i = 0; i < students.length; i++) {
+    console.log(`Great to see you, ${students[i]}!`)
+}
