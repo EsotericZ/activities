faf41b1d81100f15dbd1843f8a4e621fc52a1cfc
EsotericZ <cjsand03@gmail.com>
2021-10-30 11:27:55 -0600

    solved week 03 - 18

--- week03/18-Stu_Scope/solved/script.js
+++ week03/18-Stu_Scope/solved/script.js
@@ -1,4 +1,6 @@
 // TODO: Declare variable 'shout' with the value 'Shout' so it's available to `justShout` and `shoutItAllOut` functions
+var shout = 'Shout'
+
 function justShout() {
   console.log(shout + ", " + shout);
   return;
@@ -20,6 +22,7 @@ function sayLions() {
 }
 
 function sayTigers() {
+  var animal = "Tigers"
   console.log("and " + animal + " and "); 
   return;
 }
@@ -27,8 +30,7 @@ function sayTigers() {
 // TODO: The variable 'bears' should only declared once and 'sayBears' should return "Bears! OH MY!". 
 var bears = "Bears";
 
-function sayBears() {
-    var bears = "Pandas";
+function sayBears() { 
     console.log(bears + "! OH MY!");
     return;
 }
@@ -38,9 +40,9 @@ sayTigers();
 sayBears();
 
 // TODO: The variable 'sing' should be declared once in the local scope.
-var sing = "Sing"; 
 
 function singAlong() {
+  var sing = "Sing"; 
   console.log(sing + ",");
   var singASong = function () {
     console.log(sing + " a Song.");
