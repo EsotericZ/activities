dd08427be6105440bc6dfa9ded72e3f3bfb61672
EsotericZ <cjsand03@gmail.com>
2021-11-02 20:21:53 -0600

    Solved week 04 - 02/04

--- week04/02-Stu_Window-Object/solved/script.js
+++ week04/02-Stu_Window-Object/solved/script.js
@@ -1,12 +1,15 @@
 // TODO: What will the following line of code log?
+// GLOBAL WINDOW OBJECT
 console.log(window);
 
 // TODO: What will the following line of code log?
+// THE DOM OBJECT
 console.log(window.document);
 
 // TODO: What will the following line of code log?
+// THE ACTUAL HTML ELEMENT
 console.log(document.documentElement);
 
 // TODO: What will the following line of code log?
+// HTML FOR THE HEAD TAG
 console.log(document.head);
-
