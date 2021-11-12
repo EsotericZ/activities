9ed5c489c3e26f5039581b8793ca17f41af0f7b1
EsotericZ <cjsand03@gmail.com>
2021-11-11 19:50:36 -0700

    solved week 05-08

--- week05/08-Stu_Dom-Traversal/solved/assets/js/script.js
+++ week05/08-Stu_Dom-Traversal/solved/assets/js/script.js
@@ -1,6 +1,9 @@
 // Assemble: Create/select DOM elements
 var rootEl = $('#root');
 
+rootEl.css({background: 'blue'});
 // TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
-
+$('.item-a3').text('O');
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+$('.item-c3').text('X');
+$('.item-a1').text('O');
