e7c34144322814a09ce7aeeed3caa20c867c6ee9
EsotericZ <cjsand03@gmail.com>
2021-11-18 19:42:20 -0700

    solved week 06 12. 14

--- week06/04-Stu_Create_Fetch/Unsolved/assets/js/script.js
+++ week06/04-Stu_Create_Fetch/Unsolved/assets/js/script.js
@@ -5,7 +5,8 @@ var fetchButton = document.getElementById('fetch-button');
 
 function getApi() {
   // Insert the API url to get a list of your repos
-  var requestUrl = '';
+  // var requestUrl = 'https://api.github.com/EsotericZ/repos';
+  var requestUrl = 'https://api.github.com/users/EsotericZ/repos';
 
   fetch(requestUrl)
     .then(function (response) {
