73599bd88ec965505ecec8060e4c3e8d7ace8466
EsotericZ <cjsand03@gmail.com>
2021-11-20 10:59:37 -0700

    week 06 removed instructor activities

--- week06/20-Stu_Document_Location/Unsolved/assets/js/script.js
+++ week06/20-Stu_Document_Location/Unsolved/assets/js/script.js
@@ -2,6 +2,10 @@ var badRequestUrl = 'https://api.github.com/unicorns';
 var redirectUrl = './404.html';
 
 fetch(badRequestUrl).then(function (response) {
+  console.log(response.status)
+  if (response.status === 404) {
+      location.href = redirectUrl;
+  }
   // Use a conditional to check the response status.
   // If that status equals the conditional, then redirect to the 404 page.
 });
