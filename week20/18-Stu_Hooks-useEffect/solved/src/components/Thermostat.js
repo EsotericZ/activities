6a5a336b372023cc802164c38a2e07cd64cdbd5e
EsotericZ <cjsand03@gmail.com>
2022-03-15 21:04:05 -0600

    solved week 20-18

---
+++ week20/18-Stu_Hooks-useEffect/solved/src/components/Thermostat.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+
+function Thermostat() {
+  const [temp, setTemp] = useState(0);
+
+  useEffect(() => {
+    document.title = `Current Temp: ${temp}° F`;
+  });
+
+  const increaseTemp = () => {
+    setTemp(temp + 1);
+  };
+
+  const decreaseTemp = () => {
+    setTemp(temp - 1);
+  };
+
+  return (
+    <div className="card text-center">
+      <div className="card-header bg-warning text-white">
+        Building Temperature
+      </div>
+      <div className="card-body">
+        <p className="card-text">
+          Current Temperature: {temp}° Fahrenheit
+        </p>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={increaseTemp}
+        >
+          Raise temperature
+        </button>{' '}
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={decreaseTemp}
+        >
+          Lower temperature
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Thermostat;
